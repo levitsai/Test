1395a2b66c1e78795229e0646b0c951d4e20eda2
Levi Tsai <pisces.lord@gmail.com>
2017-05-11 11:08:00 +0800

    master-s-1
    master-d-1

--- Test/MasterViewController.swift
+++ Test/MasterViewController.swift
@@ -25,6 +25,8 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        print("~!@#$%^&")
     }
 
     override func viewWillAppear(_ animated: Bool) {
